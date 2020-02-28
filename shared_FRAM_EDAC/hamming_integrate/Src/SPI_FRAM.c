16b9997d45f5d0a54e24908ef4ece75015d44e69
slyliu@ucsc.edu <sloanyianliu@gmail.com>
2020-02-28 12:59:09 -0800

    fixed some stuff

--- shared_FRAM_EDAC/hamming_integrate/Src/SPI_FRAM.c
+++ shared_FRAM_EDAC/hamming_integrate/Src/SPI_FRAM.c
@@ -1,13 +1,5 @@
 #include "SPI_FRAM.h"
-<<<<<<< HEAD
 
-/**
-  * @brief  Read from the FRAM
-=======
-//to read 
-
-/**
->>>>>>> a58ad3d98ba432c8b55e2fb849f72bc8b7c865e4
   * @param  address: 16 bit address
   * @param  pRxData: pointer to reception data buffer to be
   * @param  size: size of the data you are expecting (# of bytes)
@@ -22,13 +14,7 @@ void SPI_FRAM_Read( SPI_HandleTypeDef *hspi,uint16_t address, uint8_t *pRxData, 
 
 	// initialize read command
 	uint8_t read_command[READ_CMD_LEN] = {READ_OP, 			// read operation code
-<<<<<<< HEAD
-										address>>8,			// MSB of address
-										(uint8_t)address}; 	// LSB of address
-=======
-																				address>>8,			// MSB of address
-																				(uint8_t)address}; 	// LSB of address
->>>>>>> a58ad3d98ba432c8b55e2fb849f72bc8b7c865e4
+
 
 	// initiate read operation
 	HAL_SPI_Transmit(hspi, read_command, READ_CMD_LEN, 10);	
@@ -45,13 +31,7 @@ void SPI_FRAM_Read( SPI_HandleTypeDef *hspi,uint16_t address, uint8_t *pRxData, 
 	// Free the lock
 	Free_Lock(hspi, huart);
 }
-<<<<<<< HEAD
-/**
-  * @brief  Write to the FRAM
-=======
-//to write
-/**
->>>>>>> a58ad3d98ba432c8b55e2fb849f72bc8b7c865e4
+
   * @param  address: 16 bit address
   * @param  pTxData: pointer to reception data buffer to be
   * @param  size: size of the data you are Transmiting (# of bytes)
@@ -66,15 +46,6 @@ void SPI_FRAM_Write(SPI_HandleTypeDef *hspi, uint16_t address, uint8_t *pTxData,
 	
 	// initialize write command bytes
 	uint8_t write_command[WRITE_CMD_LEN] = {WREN_OP, 			// enable write operation
-<<<<<<< HEAD
-											WRITE_OP,			// write operation code
-											address>>8,			// MSB of address
-											(uint8_t)address}; 	// LSB of address
-=======
-																					WRITE_OP,									// write operation code
-																					address>>8,								// MSB of address
-																					(uint8_t)address}; 				// LSB of address
->>>>>>> a58ad3d98ba432c8b55e2fb849f72bc8b7c865e4
 
 	// initiate write operation
 	HAL_SPI_Transmit(hspi, &write_command[0], 1, 10);
@@ -111,7 +82,7 @@ void SPI_FRAM_Write(SPI_HandleTypeDef *hspi, uint16_t address, uint8_t *pTxData,
 	Free_Lock(hspi, huart);
 }
 
-<<<<<<< HEAD
+
 /**
   * @brief  Get the lock to reserve access to the FRAM, like mutex_lock
   * @param  The SPI connection to the FRAM
@@ -139,32 +110,14 @@ void Get_Lock(SPI_HandleTypeDef *hspi, UART_HandleTypeDef *huart)
 		# if (DEBUG) 
 			HAL_UART_Transmit(huart, (uint8_t *)msg1, strlen(msg1), 1); 
 		#endif
-=======
 
-void Get_Lock(SPI_HandleTypeDef *hspi, UART_HandleTypeDef *huart)
-{
-	char *msg1 = "\nWAITING FOR LOCK.....\n";
-	
-	do
-	{
-		HAL_UART_Transmit(huart, (uint8_t *)msg1, strlen(msg1), 1);
-		HAL_Delay(500);
-	} while (HAL_GPIO_ReadPin(GPIOA, SPI_FRAM_IN1_Pin) == GPIO_PIN_RESET || 
-														HAL_GPIO_ReadPin(GPIOB, SPI_FRAM_IN2_Pin) == GPIO_PIN_RESET);
-	
-	HAL_GPIO_WritePin(GPIOA, SPI_FRAM_LOCK_Pin, GPIO_PIN_RESET);
-	while(HAL_GPIO_ReadPin(GPIOA, SPI_FRAM_IN1_Pin) == GPIO_PIN_RESET || 
-														HAL_GPIO_ReadPin(GPIOB, SPI_FRAM_IN2_Pin) == GPIO_PIN_RESET)
-	{
-		HAL_UART_Transmit(huart, (uint8_t *)msg1, strlen(msg1), 1);
->>>>>>> a58ad3d98ba432c8b55e2fb849f72bc8b7c865e4
 		HAL_GPIO_WritePin(GPIOA, SPI_FRAM_LOCK_Pin, GPIO_PIN_SET);
 		for(int i = 0; i < 1000; i++);
 		HAL_GPIO_WritePin(GPIOA, SPI_FRAM_LOCK_Pin, GPIO_PIN_RESET);
 	}
 }
 
-<<<<<<< HEAD
+
 /**
   * @brief  Free the lock that is being held to read or write, like mutex_unlock
   * @param  The SPI connection to the FRAM
@@ -176,13 +129,7 @@ void Free_Lock(SPI_HandleTypeDef *hspi, UART_HandleTypeDef *huart)
 		char *msg1 = "\nFreeing lock.....\n";
 		HAL_UART_Transmit(huart, (uint8_t *)msg1, strlen(msg1), 1);
 	#endif
-=======
-void Free_Lock(SPI_HandleTypeDef *hspi, UART_HandleTypeDef *huart)
-{
-	char *msg1 = "\nFreeing lock.....\n";
 
-	HAL_UART_Transmit(huart, (uint8_t *)msg1, strlen(msg1), 1);
->>>>>>> a58ad3d98ba432c8b55e2fb849f72bc8b7c865e4
 	
 	HAL_GPIO_WritePin(GPIOA, SPI_FRAM_LOCK_Pin, GPIO_PIN_SET);	
 }
