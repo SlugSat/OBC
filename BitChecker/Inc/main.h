55ddc1bf30f1694f2eaa9cd3778d2cf4f8ed4a3e
Myunggeun Kang <mykang@ucsc.edu>
2020-05-18 12:50:54 -0700

    wee

---
+++ BitChecker/Inc/main.h
@@ -0,0 +1,87 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define Third_Pin GPIO_PIN_1
+#define Third_GPIO_Port GPIOB
+#define Fourth_Pin GPIO_PIN_2
+#define Fourth_GPIO_Port GPIOB
+#define Fifth_Pin GPIO_PIN_11
+#define Fifth_GPIO_Port GPIOB
+#define Sixth_Pin GPIO_PIN_12
+#define Sixth_GPIO_Port GPIOB
+#define Zero_Pin GPIO_PIN_13
+#define Zero_GPIO_Port GPIOB
+#define First_Pin GPIO_PIN_14
+#define First_GPIO_Port GPIOB
+#define Second_Pin GPIO_PIN_15
+#define Second_GPIO_Port GPIOB
+#define Seventh_Pin GPIO_PIN_11
+#define Seventh_GPIO_Port GPIOA
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ BitChecker/MDK-ARM/BitChecker/startup_stm32l152xe.d
@@ -0,0 +1,1 @@
+bitchecker\startup_stm32l152xe.o: startup_stm32l152xe.s
